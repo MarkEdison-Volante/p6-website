5d4e25b0b29e471e0ba07621c56955d2843e0082
Mark Edison Volante <mevolante@gbox.ncf.edu.ph>
2025-12-02 11:06:21 +0800

    Change Hobbies header color to yellow

--- src/app/hobbies/page.tsx
+++ src/app/hobbies/page.tsx
@@ -13,7 +13,7 @@ export default function Hobbies() {
         <Image src="/images/celestegif.gif" layout="fill" alt="celestebg" className="z-[-1]" unoptimized = {true}/>
 
         <section className="py-10">
-            <h1 style={{ fontSize: "35px"}}><b>Hobbies</b></h1>
+            <h1 style={{ fontSize: "35px", color: "yellow"}}><b>Hobbies</b></h1>
             <br/>
             <div style={{color: "white"}}>
             <p>
